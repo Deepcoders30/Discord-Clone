9b48501b49c56e36ddaf235632ba37135986a9ae
Deepcoders30 <umair.khan.rocko@gmail.com>
2023-09-05 00:59:55 +0530

    Initial Modal UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -14,11 +15,11 @@ const SetupPage = async () => {
     },
   });
 
-  if(server){
-    return redirect(`/servers/${server.id}/`)
+  if (server) {
+    return redirect(`/servers/${server.id}/`);
   }
 
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
